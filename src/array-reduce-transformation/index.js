26cbbfec0073426e1a7cb2d8e79004a3bff94ef1
AliaksandrHrychonak <@icloud.com>
2025-07-28 15:24:41 +0300

    feat: test

--- src/array-reduce-transformation/index.js
+++ src/array-reduce-transformation/index.js
@@ -16,8 +16,6 @@ const reduce = (nums, fn, init) => {
   return acc
 };
 
-
-
 console.log(reduce([1,2,3,4], function sum(accum, curr) { return accum + curr; }, 0))
 console.log(reduce([1,2,3,4], function sum(accum, curr) { return accum + curr * curr; }, 0))
 console.log(reduce([], function sum(accum, curr) { return 0; }, 25))
