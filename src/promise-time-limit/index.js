3a4f84c537225031ac89edf3e91ba7b74fb10264
AliaksandrHrychonak <alexgrichenok@icloud.com>
2025-08-13 23:07:26 +0300

    chore(promise-time-limit): remove redundant newlines

--- src/promise-time-limit/index.js
+++ src/promise-time-limit/index.js
@@ -22,6 +22,8 @@ const timeLimit = (fn, t) => {
     });
   };
 };
+
+
 /**
  * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
  * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
